707e6ae1f71b6d5af4600bfc0fe277674ecca311
Ralf Bartsch <34740220+rb4r@users.noreply.github.com>
2018-09-05 15:37:52 +0200

    Initial upload

---
+++ src/CSVfitting.cpp
@@ -0,0 +1,359 @@
+/*******************************************************************************
+*
+*  CSVfitting - by Ralf Bartsch
+*  ============================
+*  Verarbeitet eine CSV-Datei aus Messwerten durch Zusammenfassen von einer
+*  festgelegten Anzahl von Zeilen oder Umwandeln jeder einzeln Zeile. Dabei
+*  kann für jede Spalte angegeben werden wie diese bearbeitet werden soll.
+*
+*  Versionshistorie:
+*  -----------------
+*  1.0.0 : 21.03.2012
+*    - erste Version
+*    - ToDo: Datum/Zeit-Verarbeitung
+*
+*
+*******************************************************************************/
+
+#include <iostream>	// Dateiein- und -ausgaben
+#include <string>		// Verwendung von String
+#include <ctime>		// Zeit
+#include <sstream>	// Verarbeitung von Strings
+#include <fstream>	// Dateioperationen
+#include <vector>		// Verwendung von Vektoren
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include "rb_func.cpp"
+using namespace std;
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Programmspezifische Funktionen
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+const int ERR_CONFIG_NOT_FOUND	= 1;
+const int ERR_INPUT_NOT_FOUND		= 2;
+const int ERR_OUTPUT_NOT_OPENED	= 3;
+const int ERR_INOUPUT_SAME			= 4;
+
+const int DEF_COUNT	= 6;
+const int DEF_TYPE	= 0;
+const int DEF_PROC	= 1;
+const int DEF_FACT	= 2;
+const int DEF_PREC	= 3;
+const int DEF_TRIM	= 4;
+const int DEF_QUOTE	= 5;
+
+const int COL_NUM  = 0;
+const int COL_TEXT = 1;
+const int COL_DATE = 2;
+const int COL_TIME = 3;
+const int COL_DEL  = 4;
+const int PROC_FIRST = 11;
+const int PROC_LAST  = 12;
+const int PROC_AMEAN = 13;
+const int PROC_QMEAN = 14;
+const int PROC_GMEAN = 15;
+const int PROC_MIN   = 16;
+const int PROC_MAX   = 17;
+const int PROC_SUM   = 18;
+const int PROC_DIFF  = 19;
+const int PROC_CLEAR = 20;
+
+int ColType(string Str) {
+	if (Str=="num")  return COL_NUM;
+	if (Str=="date") return COL_DATE;
+	if (Str=="time") return COL_TIME;
+	if (Str=="del")  return COL_DEL;
+	return COL_TEXT;
+}
+int ColProc(string Str) {
+	if (Str=="first") return PROC_FIRST;
+	if (Str=="amean") return PROC_AMEAN;
+	if (Str=="qmean") return PROC_QMEAN;
+	if (Str=="gmean") return PROC_GMEAN;
+	if (Str=="min")   return PROC_MIN;
+	if (Str=="max")   return PROC_MAX;
+	if (Str=="sum")   return PROC_SUM;
+	if (Str=="diff")  return PROC_DIFF;
+	if (Str=="clear") return PROC_CLEAR;
+	return PROC_LAST;
+}
+
+string ProcessBundle(vector<string> Col, vector<string> ColDef, string Comma, string PrevBundleValue) {
+	unsigned int i;
+	double res = 0;
+	string sres = "";
+	unsigned int Type	= ColType(ColDef[DEF_TYPE]);
+	unsigned int Proc = ColProc(ColDef[DEF_PROC]);
+	double Factor			= StringToDouble(ColDef[DEF_FACT]);
+	int Precision			= StringToInt(ColDef[DEF_PREC]);
+	bool Trim					= (ColDef[DEF_TRIM]=="yes" or ColDef[DEF_TRIM]=="1") ? true : false;
+	bool Quote				= (ColDef[DEF_QUOTE]=="yes" or ColDef[DEF_QUOTE]=="1") ? true : false;
+
+	switch(Type) {
+		case COL_TEXT: {
+			if (Proc==PROC_FIRST) {
+				sres = Col[0];
+			} else if (Proc==PROC_CLEAR) {
+				sres = "";
+			} else { //Proc=LAST
+				sres = Col[Col.size()-1];
+			}
+			if (Quote) return AddQuotes(sres);
+			return sres;
+			break;
+		}
+		case COL_DATE: {
+			if (Proc==PROC_FIRST) {
+				sres = Col[0];
+			} else if (Proc==PROC_CLEAR) {
+				sres = "";
+			} else { //Proc=LAST
+				sres = Col[Col.size()-1];
+			}
+			if (Quote) return AddQuotes(sres);
+			return sres;
+			break;
+		}
+		case COL_TIME: {
+			if (Proc==PROC_FIRST) {
+				sres = Col[0];
+			} else if (Proc==PROC_CLEAR) {
+				sres = "";
+			} else { //Proc=LAST
+				sres = Col[Col.size()-1];
+			}
+			if (Quote) return AddQuotes(sres);
+			return sres;
+			break;
+		}
+		case COL_NUM: {
+			if (Proc==PROC_FIRST) {
+				res = StringToDouble(Col[0]);
+			}
+			if (Proc==PROC_LAST) {
+				res = StringToDouble(Col[Col.size()-1]);
+			}
+			if (Proc==PROC_AMEAN or Proc==PROC_SUM) {
+				for (i=0; i<Col.size(); i++) {res+=StringToDouble(Col[i]);}
+				if (Proc==PROC_AMEAN) res = res / Col.size();
+			}
+			if (Proc==PROC_QMEAN) {
+				for (i=0; i<Col.size(); i++) {res+=pow(StringToDouble(Col[i]),2);}
+				res = res / Col.size();
+			}
+			if (Proc==PROC_GMEAN) {
+				res = 1;
+				for (i=0; i<Col.size(); i++) {res*=StringToDouble(Col[i]);}
+				res = pow(res,1/Col.size());
+			}
+			if (Proc==PROC_MIN) {
+				res = StringToDouble(Col[0]);
+				for (i=1; i<Col.size(); i++) {
+					if (StringToDouble(Col[i]) < res) res = StringToDouble(Col[i]);
+				}
+			}
+			if (Proc==PROC_MAX) {
+				res = StringToDouble(Col[0]);
+				for (i=1; i<Col.size(); i++) {
+					if (StringToDouble(Col[i]) > res) res = StringToDouble(Col[i]);
+				}
+			}
+			if (Proc==PROC_DIFF) {
+				res = StringToDouble(Col[Col.size()-1]) - StringToDouble(PrevBundleValue);
+			}
+			if (Proc==PROC_CLEAR) {
+				res = 0;
+			}
+			sres = DoubleToString(res*Factor, Precision);
+			i = sres.find(".");
+			if (i!=string::npos) sres.replace(i,1,Comma);
+			return sres;
+			break;
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//   Hauptprogramm
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char **argv) {
+	string config_filename		= "";
+	string input_filename			= "";
+	string output_filename		= "";
+	string import_separator		= ";";
+	string export_separator		= ";";
+	string export_comma	   		= ",";
+	unsigned int start_line		= 1;
+	unsigned int bundle_rows	= 5;
+	unsigned int bundle_cols	= 1;
+	vector< vector<string> > column_def;
+	vector<string> vstring;
+	string line;
+	unsigned int i;
+	unsigned int processed_lines = 0;
+
+	/*############################################################################
+	#
+	#  Konfiguration einlesen
+	#
+	############################################################################*/
+
+	config_filename = ExtractFilenamePart(argv[0],true,true,false) + ".ini";
+	if (argc>1) {
+		if (file_exists(argv[1])) {config_filename = argv[1];}
+	}
+	ifstream config_file;
+	config_file.open(config_filename.c_str(), ios::in);
+	if (!config_file.is_open()) {
+		// Programm abbrechen, weil Datei nicht gefunden
+		cout << "Konfigurationsdatei '" << config_filename << "' nicht gefunden... Abgebrochen" << endl;
+		return ERR_CONFIG_NOT_FOUND;
+	} else {
+		// Parameter auslesen
+		vector<string> entries;
+    while (getline(config_file, line)) {
+        stringstream sline(line);
+        string variable, value;
+        sline>>variable>>value;
+        if (variable=="import_separator")		{import_separator=value;}
+        if (variable=="export_separator")		{export_separator=value;}
+        if (variable=="export_comma")				{export_comma=value;}
+        if (variable=="start_line")					{start_line=StringToInt(value);}
+        if (variable=="row_bundle")					{bundle_rows=StringToInt(value);}
+        if (variable=="input_file") {
+					if (ExtractFilenamePart(value,true,false,false)=="") {value = ExtractFilenamePart(argv[0],true,false,false)+value;}
+        	input_filename = value;
+				}
+        if (variable=="output_file") {
+					if (ExtractFilenamePart(value,true,false,false)=="") {value = ExtractFilenamePart(argv[0],true,false,false)+value;}
+        	output_filename = value;
+				}
+				if (variable=="column") {
+					for (i=CountSubstr(value,","); i<DEF_COUNT; i++) {value+=",";}
+					SplitString(value, ",", entries);
+					column_def.push_back(entries);
+				}
+		}
+		bundle_cols = column_def.size();
+		config_file.close();
+	}
+
+	//cout << start_line << " / " << precision << " / " << import_separator << " / " << bundle_rows << " / " << bundle_cols << endl;
+	//for (i=0; i<bundle_cols; i++) cout << bundle_type[i] << "-" << bundle_proc[i] << "-" << bundle_factor[i] << endl;
+
+	/*############################################################################
+	#
+	#  Namen der Eingabe- und Ausgabedatei festlegen, und beide öffnen.
+	#
+	############################################################################*/
+
+	cout << endl;
+	cout << "Eingabedatei: " << input_filename << " -> " << ((file_exists(input_filename))?"OK":"Nicht gefunden.") << endl;
+	cout << "Ausgabedatei: " << output_filename << " -> " << ((file_exists(output_filename))?"Wird überschrieben.":"OK") << endl;
+	cout << endl;
+
+	// Eingabedatei öffnen. Bei Fehler Abbruch des Programms
+	ifstream input_file;
+	input_file.open(input_filename.c_str(), ios::in);		// öffnet Datei im Lesemodus
+	if (!input_file.is_open()) {												// Programm abbrechen, weil Datei nicht gefunden
+		cout << "Eingabedatei nicht gefunden... Abgebrochen" << endl;
+		return ERR_INPUT_NOT_FOUND;
+	}
+
+	// Ausgabedatei erstellen und zum Schreiben öffnen. Bei Fehler Abbruch des Programms
+	ofstream output_file(output_filename.c_str(), ios::out); // öffnet Datei im Schreibmodus
+	if (!output_file.is_open()) {
+		cout << "Konnte Ausgabedatei nicht erstellen.... Abgebrochen" << endl;
+		return ERR_OUTPUT_NOT_OPENED;
+	}
+
+	if (input_filename==output_filename) {
+		cout << "Ein- und Ausgabedatei sind die selben... Abgebrochen" << endl;
+		return ERR_INOUPUT_SAME;
+	}
+
+	/*############################################################################
+	#
+	#  Umwandlung der CSV-Datei
+	#
+	############################################################################*/
+
+	// Initialisieren
+	cout << "Starte Umwandlung..." << endl;
+	vector<string> cols;
+	vector< vector<string> > bundle;
+	vector<string>* col;
+	unsigned int bundle_count = 0;
+	string bundle_result;
+	// 2D-Vektor initialisieren
+	for (i=0; i<bundle_cols; i++) {
+		bundle.push_back(vector<string>());
+	}
+
+	// Header aus Eingabedate in Ausgabedatei übernehmen
+	for (i=0; i<start_line-1; i++) {
+		if (getline(input_file, line)) {
+			output_file << line << endl;
+		}
+	}
+	// Datenzeilen verarbeiten
+	while (getline(input_file, line)) {
+		bundle_count++;
+		// Zeile in Vector schreiben
+		SplitString(line, import_separator, cols);
+		// Einträge säubern
+		for (i=0; i<cols.size(); i++) {cols[i] = Trim(cols[i]);}
+		// Bundle neu beginnen
+		if (bundle_count==1) {
+			for (i=0; i<bundle_cols; i++) {bundle[i].clear();}
+		}
+		// Zeile dem Bundle hinzufügen
+		for (i=0; i<bundle_cols; i++) {bundle[i].push_back(cols[i]);}
+		// Bundle abschließen
+		if (bundle_count==bundle_rows) {
+			bundle_result = "";
+			for (i=0; i<bundle_cols; i++) {
+				if (ColType(column_def[i][DEF_TYPE])!=COL_DEL) {
+					bundle_result += ProcessBundle(bundle[i], column_def[i], export_comma, "") + export_separator;
+				}
+			}
+			output_file << bundle_result.substr(0,bundle_result.length()-1) << endl;
+			bundle_count = 0;
+		}
+		processed_lines++;
+	}
+	if (bundle_count!=0) {
+		bundle_result = "";
+		for (i=0; i<bundle_cols; i++) {
+			if (ColType(column_def[i][DEF_TYPE])!=COL_DEL) {
+				bundle_result += ProcessBundle(bundle[i], column_def[i], export_comma, "") + export_separator;
+			}
+		}
+		output_file << bundle_result.substr(0,bundle_result.length()-1) << endl;
+	}
+
+	/*############################################################################
+	#
+	#  Beenden des Programm
+	#
+	############################################################################*/
+
+	input_file.close();
+	output_file.close();
+	cout  << processed_lines << " verarbeitete Zeilen." << endl;
+	cout << "Umwandlung abgeschlossen." << endl << endl;
+	return 0;
+}
---
+++ src/rb_func.cpp
@@ -0,0 +1,295 @@
+/*******************************************************************************
+*
+*  Eigene Funktionen
+*  ^^^^^^^^^^^^^^^^^
+*  Stand: 21.03.2012
+*
+*******************************************************************************/
+
+#include <string>		// Verwendung von String
+#include <sstream>	// Verarbeitung von Strings
+#include <iostream>	// Dateieingabe und -ausgabe
+#include <ctime>		// Zeit
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cfloat>
+using namespace std;
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//   Mathematische Funktionen
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/* Symmetric arithmetic rounding, n in -15..15 */
+double round(double x, int n) {
+	bool neg = (x < 0.0);
+	double ipart, fpart;
+	double y, p;
+	int id;
+
+	if (neg) x = -x;
+	if (n > 0) {
+		double yy;
+		fpart = modf(x,&ipart);
+		if (n > DBL_DIG) n = DBL_DIG;
+		p = pow(10,n);
+		y = fpart * p;
+		fpart = modf(y,&yy);
+		if (fpart < 0.5) yy += 1.0;
+		y = ipart + yy / p;
+	}	else if (n < 0) {
+		if (n < -DBL_DIG)
+		n = -DBL_DIG;
+		p = pow(10, -n);
+		y = x / p;
+		y = round(y,0) * p;
+	}	else { /* n == 0 */
+		fpart = modf(x,&ipart);
+		y = (fpart < 0.5)? ipart: ipart + 1;
+	}
+	return neg?-y:y;
+}
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//   Umwandlungsfunktionen
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+string IntToString(int val) {
+	ostringstream outStream;
+	outStream << val;
+	return outStream.str();
+}
+int StringToInt(string val) {
+	int i;
+	istringstream inStream(val);
+	inStream >> i;
+	return i;
+}
+string TimeToSQLString(time_t val) {
+	struct tm *ptm;
+	char   buffer[80];
+	ptm = gmtime(&val);
+	strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", ptm);
+	return buffer;
+}
+time_t SQLStringToTime(string val) {
+	struct tm dt;
+	dt.tm_year = StringToInt(val.substr(0,4));
+	dt.tm_mon  = StringToInt(val.substr(5,2));
+	dt.tm_mday = StringToInt(val.substr(8,2));
+	dt.tm_hour = StringToInt(val.substr(11,2));
+	dt.tm_min  = StringToInt(val.substr(14,2));
+	dt.tm_sec  = StringToInt(val.substr(17,2));
+	return mktime(&dt);
+}
+char* StringToChars(string val) {
+	char* res = new char[256];
+	strcpy(res, val.c_str());
+	return res;
+}
+string DoubleToString(double value, const int precision = -1) {
+	int n;
+	char buffer[64];
+	char format[16];
+	std::stringstream ss;
+
+	if (precision<0) {
+		ss << "%f";
+	} else {
+		ss << "%0." << precision << "f";
+	}
+	ss >> format;
+	n = sprintf(buffer, format, value);
+	return buffer;
+}
+double StringToDouble(string value, const int precision = -1) {
+	double d;
+	int pos;
+
+	pos = value.find(",");
+	if (pos!=string::npos) {value.replace(pos,1,".");}
+
+	if (precision<0) {
+		d = atof(value.c_str());
+	} else {
+		pos = value.find(".");
+		if (pos!=string::npos) {
+			value = value.substr(0, pos+1+precision);
+			d = atof(value.c_str());
+		} else {
+			d = atof(value.c_str());
+		}
+	}
+	return d;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//   Hilfsfunktionen
+//
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+bool file_exists(const string& filename) {
+  ifstream file(filename.c_str());
+  bool result = file.good();
+  file.close();
+  return result;
+}
+string ExtractFilenamePart(const string FileName, const bool Path, const bool Name, const bool Extension) {
+	// extrahiert die angegebenen Teile eines Dateinamens
+	string spath = "", sname = "", sext = "";
+	int ibackslash	= FileName.find_last_of("\\");
+	int ipoint 			= FileName.find_last_of(".");
+
+	if (ibackslash!=string::npos) {
+		spath = FileName.substr(0,ibackslash+1);
+		if (ipoint!=string::npos) {
+			sname	= FileName.substr(ibackslash+1,ipoint-ibackslash-1);
+			sext	= FileName.substr(ipoint+1);
+		} else {
+			sname	= FileName.substr(ibackslash+1);
+		}
+	} else {
+		if (ipoint!=string::npos) {
+			sname	= FileName.substr(0,ipoint);
+			sext	= FileName.substr(ipoint+1);
+		} else {
+			sname = FileName;
+		}
+	}
+
+	if (!Path)			spath = "";
+	if (!Name)			sname = "";
+	if (!Extension)	sext  = "";
+	if (Name and Extension) sext = "." + sext;
+
+	return spath+sname+sext;
+}
+int CountSubstr(string Str, string Chars) {
+	int pos = 0;
+	int count = 0;
+	do {
+		pos = Str.find(Chars, pos);
+		if (pos!=string::npos) {
+			count++;
+			pos += Chars.length();
+		}
+	} while (pos!=string::npos);
+	return count;
+}
+string RemoveQuotes(string Str) {
+	if (Str.substr(0,1)=="\"" and Str.substr(Str.length()-1,1)=="\"") {
+		return Str.substr(1,Str.length()-2);
+	} else {
+		return Str;
+	}
+}
+string AddQuotes(string Str) {
+	return "\"" + Str + "\"";
+}
+string BoolToString(const bool val, const string true_str, const string false_str) {
+	if (val==true) {
+		return true_str;
+	} else {
+		return false_str;
+	}
+}
+void SplitCSVLine(string CSVLine, char Separator, vector<string>& Tokens) {
+	// Trennt eine Zeile anhand eines(!) Zeichens
+	string elem = "";
+	string temp;
+	istringstream sline(CSVLine);
+	Tokens.clear();
+
+	while (true) {
+		temp = "";
+		// Text bis zum ersten Komma einlesen
+		if (getline(sline,temp,Separator)) {
+			elem += temp;
+		} else {
+			// Zeilenende -> letztes Element in Vector schreiben
+			if(!elem.empty()) Tokens.push_back(RemoveQuotes(elem));
+			return;
+		}
+		// Prüfen ob Anführungszeichen (") im Element ist
+		if ((CountSubstr(elem,"\"") % 2)==0) {
+			// gerade Anzahl an Anführungszeichen -> Element komplett
+			Tokens.push_back(RemoveQuotes(elem));
+			// Elementstring zurücksetzen
+			elem = "";
+		} else {
+			// ungerade Anzahl -> Komma Teil der Zeichenkette -> weiter in der Schleife
+			elem += Separator;
+		}
+	}
+}
+void SplitString(string Str, string Separator, vector<string>& Tokens) {
+	// Trennt einen String einhand einer Zeichenfolge (auch mehr wie 1 Zeichen)
+	// Der String kann sich auch über mehre Zeilen erstrecken
+	// Anführungszeichen werden mit beachtet
+	istringstream sline(Str);
+	Tokens.clear();
+	string elem = "";
+	int seplen = Separator.length();
+	if (Separator.substr(0,1)=="\\") {
+		if (Separator=="\\t") {
+			seplen = 1; Separator="\t";
+		} else {
+			cout << endl << "Achtung: SplitString(..) verwendet ein unbekanntes Escape-Zeichen" << endl;
+		}
+	}
+	int strlen = Str.length();
+	int pos = 0;
+	int lastpos = -seplen;
+
+	while (pos+seplen<=strlen) {
+		// Text bis zum ersten Separator einlesen
+		if (Str.substr(pos,seplen)==Separator) {
+			elem = Str.substr(lastpos+seplen,pos-lastpos-seplen);
+			if (elem.substr(0,1)=="\"") {
+				// Element mit Anführungszeichen -> nur hinzufügen wenn auch Anführungszeichen hinten
+				if (elem.substr(elem.length()-1)=="\"") {
+					Tokens.push_back(RemoveQuotes(elem));
+					elem = "";
+					lastpos = pos;
+				} else {
+					pos++;
+				}
+			} else {
+				// Element hinzufügen
+				Tokens.push_back(RemoveQuotes(elem));
+				elem = "";
+				lastpos = pos;
+			}
+			pos += seplen;
+		} else {
+			pos++;
+		}
+	}
+	elem = Str.substr(lastpos+seplen);
+	Tokens.push_back(RemoveQuotes(elem));
+}
+string& TrimLeft(string& str, const char* delimiters = " \t\n\r\f\v") {
+	str.erase(0, str.find_first_not_of(delimiters));
+	return str;
+}
+string& TrimRight(string& str, const char* delimiters = " \t\n\r\f\v") {
+	str.erase(str.find_last_not_of(delimiters) + 1);
+	return str;
+}
+string& Trim(string& str, const char* delimiters = " \t\n\r\f\v") {
+	str.erase(0, str.find_first_not_of(delimiters));
+	str.erase(str.find_last_not_of(delimiters) + 1);
+	return str;
+}
